f44e0087b7fa14133607d04bb1b04ec9c2296eae
pillepalle1 <benjkahl@googlemail.com>
2020-09-14 17:30:23 +0800

    Adjust IAsyncCollection Methods to be combineable with ICollection

--- Collections/IAsyncCollection.cs
+++ Collections/IAsyncCollection.cs
@@ -4,14 +4,13 @@ namespace dotnet_pillepalle1.Collections
 {
     public interface IAsyncCollection<T> : System.Collections.Generic.IAsyncEnumerable<T>
     {
-        Task Add(T item);
-        Task<bool> Remove(T item);
-        Task Clear();
-        Task<bool> Contains(T item);
-        Task CopyTo(T[] array, int arrayIndex);
+        Task AddAsync(T item);
+        Task<bool> RemoveAsync(T item);
+        Task ClearAsync();
+        Task<bool> ContainsAsync(T item);
+        Task CopyToAsync(T[] array, int arrayIndex);
 
-        Task<int> Count { get; }
-        Task<bool> IsReadOnly { get; }
-        object SyncRoot { get; }
+        Task<int> CountAsync { get; }
+        Task<bool> IsReadOnlyAsync { get; }
     }
 }
